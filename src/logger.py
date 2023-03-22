b55efdc7557c149a446e0b51736e8d07f845ec38
msaad1311 <muhammadsaad1311@gmail.com>
2023-03-22 18:48:17 -0400

    updated the logger code in order to have a colored logs

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,31 @@
 import logging
+import sys
 import os
+
 from datetime import datetime
+
+class CustomFormatter(logging.Formatter):
+
+    cyan = "\u001b[36m"
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)" # type: ignore
+
+    FORMATS = {
+        logging.DEBUG: cyan + format + reset, # type: ignore
+        logging.INFO: grey + format + reset, # type: ignore
+        logging.WARNING: yellow + format + reset, # type: ignore
+        logging.ERROR: red + format + reset, # type: ignore
+        logging.CRITICAL: bold_red + format + reset # type: ignore
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
@@ -8,8 +33,16 @@ os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
+
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    filename= LOG_FILE_PATH,
     level=logging.DEBUG,
+    format= '[%(asctime)s] %(name)s %(levelname)s - %(message)s'
+    
 )
+# set up logging to console
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+console.setFormatter(CustomFormatter())
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
